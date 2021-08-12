010044a734115016dd3a70530573b1ed0370e203
Maberry87 <michael123087@gmail.com>
2021-08-12 17:37:37 -0500

    equip bonus mission
    did equipment bonus mission

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = ["Habitat dome", "Drones","Food containers", "Oxygen tanks"]
+  
+  equipmentBeingEdited: number; //index of equipment being edited
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(equipmentName: string) {
+    this.equipment.push(equipmentName);
+  }
+
+  remove(equipmentName: string) {
+    let index = this.equipment.indexOf(equipmentName);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(equipment: string) {
+    this.equipmentBeingEdited = index;
+ }
+
+ save(equipmentName: string, index: number) {
+  this.equipment[index] = equipmentName;
+  this.equipmentBeingEdited = null;
   }
 
 }
